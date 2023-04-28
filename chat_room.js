14710f6268a111fd0308668df097f1887751c4ef
Maria17ecs <122824717+Maria17ecs@users.noreply.github.com>
2023-04-28 20:43:23 -0300

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBHAwVdu7prCNshFL9FI9Qg-9h-XEtpZu8",
+    authDomain: "web-app-vamos-conversar.firebaseapp.com",
+    databaseURL: "https://web-app-vamos-conversar-default-rtdb.firebaseio.com",
+    projectId: "web-app-vamos-conversar",
+    storageBucket: "web-app-vamos-conversar.appspot.com",
+    messagingSenderId: "267878500867",
+    appId: "1:267878500867:web:27ad4a9d420c9c5becdc36",
+    measurementId: "G-TQQBCRLY6J"
+  };
